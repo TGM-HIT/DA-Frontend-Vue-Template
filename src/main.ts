80522e7a31221a8da3d136d748e02d50e52581bd
Michael Pointner <mpointner@tgm.ac.at>
2025-01-02 22:53:14 +0100

    Init Vue app with Vuelidate, Axios, Vuetify and default content

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(createVuetify({ components, directives, theme: { defaultTheme: 'light' } }))
+app.use(router)
+
+app.mount('#app')
